b09efec3e47a9381983224f1fdf0c717f85a5f52
Antony-M2 <antony15898@gmail.com>
2025-10-26 04:16:22 +0000

    Init changes

---
+++ logistics_erp_integration_v2/logistics_erp_integration_v2/doctype/lei_company_mapping/lei_company_mapping.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, admin@strebentechnik.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class LEICompanyMapping(Document):
+	pass
