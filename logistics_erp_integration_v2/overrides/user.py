ccf92dbe7e0eade5d846041ea4d52bef751873ec
Antony-M2 <antony15898@gmail.com>
2025-10-30 16:54:21 +0000

    final unit test and code

--- logistics_erp_integration_v2/overrides/user.py
+++ logistics_erp_integration_v2/overrides/user.py
@@ -1,8 +1,13 @@
+import frappe
 from frappe.core.doctype.user.user import User
 from logistics_erp_integration_v2.utils.duplicate import (
     check_dublicate_value_details, get_child_table_changed_and_newly_added_details
 )
-from logistics_erp_integration_v2.utils.auto_user_permission import get_previous_company_mapping_details
+from logistics_erp_integration_v2.utils.auto_user_permission import (
+    get_previous_company_mapping_details,
+    create_or_update_user_permission_for_company
+)
+
 
 class CustomUser(User):
     
@@ -21,3 +26,24 @@ class CustomUser(User):
             {'company': 'Company'}
         )
         new_entries, updated_entries, delete_entries = get_child_table_changed_and_newly_added_details(previous_company_mapping_details, compay_details)
+        self.create_permission = new_entries + updated_entries
+        self.delete_permission = delete_entries
+
+    def on_update(self):
+        """
+        This method is called when the Customer document is updated & Newly Inserted
+        """
+        super().on_update()
+
+        if self.create_permission or self.delete_permission:
+            # create_or_update_user_permission_for_company(
+            #     self.create_permission, self.delete_permission, frappe.session.user
+            # )
+        
+            frappe.enqueue(
+                create_or_update_user_permission_for_company,
+                queue='default',
+                create_permission=self.create_permission,
+                delete_permission=self.delete_permission,
+                session_user=frappe.session.user
+            )
--- logistics_erp_integration_v2/utils/auto_user_permission.py
+++ logistics_erp_integration_v2/utils/auto_user_permission.py
@@ -31,3 +31,118 @@ def get_previous_company_mapping_details(
         order_by="idx"
     )
     return previous_list
+
+
+
+def create_or_update_user_permission_for_company(
+    create_permission: List[Dict],
+    delete_permission: List[Dict],
+    session_user: str
+):
+    """
+    Create or update User Permissions for the specified user based on the created and deleted company mappings.
+
+    Args:
+        create_permission (List[Dict]): A list of dictionaries containing newly created company mappings.
+        delete_permission (List[Dict]): A list of dictionaries containing deleted company mappings.
+        session_user (str): to track who is making the changes.
+    """
+    doctype = 'LEI Company Mapping'
+    if create_permission:
+        parent = list(set([cp['parent'] for cp in create_permission]))[0]
+        parenttype = list(set([cp['parenttype'] for cp in create_permission]))[0]
+        # doctype = list(set([cp['doctype'] for cp in create_permission]))[0]
+        companies = list(set([cp['company'] for cp in create_permission]))
+        processing_data, passing_parenttype = get_user_permisison_processing_data(doctype, parent, parenttype, companies)
+        create_user_permission_for_company(processing_data, parent, passing_parenttype )
+
+    if delete_permission:
+        parent = list(set([cp['parent'] for cp in delete_permission]))[0]
+        parenttype = list(set([cp['parenttype'] for cp in delete_permission]))[0]
+        # doctype = list(set([cp['doctype'] for cp in delete_permission]))[0]
+        companies = list(set([cp['company'] for cp in delete_permission]))
+        processing_data, passing_parenttype = get_user_permisison_processing_data(doctype, parent, parenttype, companies)
+        delete_user_permission_for_company(processing_data, parent, passing_parenttype)
+    
+    frappe.log_error(
+        "create_or_update_user_permission_for_company",
+        f"User Permission Updated for \ncreate_permission\n{create_permission}\ndelete_permission\n{delete_permission}",
+        reference_doctype=parenttype,
+        reference_name=parent
+    )
+
+def get_user_permisison_processing_data(doctype, parent, parenttype, companies):
+    if parenttype == 'Customer':
+        passing_parenttype = 'User'
+    elif parenttype == 'User':
+        passing_parenttype = 'Customer'
+    else:
+        frappe.log_error("get_user_permisison_processing_data", "Unexpected Parenttype only processing User and Customer")
+        raise Exception("Unexpected Parenttype only processing User and Customer")
+    
+    data = frappe.db.get_all(
+        doctype,
+        pluck='parent',
+        filters={
+            'parenttype': passing_parenttype,
+            'company': ['in', companies]
+        }
+    )
+    data = list(set(data))
+    return data, passing_parenttype
+
+
+def create_user_permission_for_company(processing_data: List[str], parent: str, passing_parenttype: str):
+    """
+    Create a User Permission for the specified user and company.
+
+    Args:
+        processing_data: List[str] - Data to create the permission
+        parent: str - main parent can be User or Customer
+        passing_parenttype: str - User or Customer
+    """
+    for pd in processing_data:
+        if passing_parenttype == 'User':
+            user = pd
+            customer = parent
+        elif passing_parenttype == 'Customer':
+            user = parent
+            customer = pd
+        else:
+            frappe.log_error("create_user_permission_for_company", f"Got different passing_parrent {passing_parenttype}")
+            raise Exception(f"Got different passing_parrent {passing_parenttype}")
+
+        doc = frappe.get_doc({
+            'doctype': 'User Permission',
+            'user': user,
+            'allow': 'Customer',
+            'for_value': customer,
+            'apply_to_all_doctypes': 1
+        })
+        doc.insert()
+        doc.add_comment('Comment', 'Created By Auto User Permission for Company Mapping')
+
+
+def delete_user_permission_for_company(processing_data: List[str], parent: str, passing_parenttype: str):
+    """
+    Remove the User Permission for the specified user and company.
+
+    Args:
+        user (str): The user whose permission will be removed.
+        company (str): The company for which the permission is removed.
+    """
+    if passing_parenttype == 'User':
+        users = processing_data
+        customers = [parent]
+    elif passing_parenttype == 'Customer':
+        users = [parent]
+        customers = processing_data
+    frappe.db.delete(
+        'User Permission',
+        filters={
+            'user': ['in', users],
+            'for_value': ['in', customers],
+            'allow': 'Customer',
+            'apply_to_all_doctypes': 1
+        }
+    )
